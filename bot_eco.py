ebae8db02d0180083e4dfe9c6f36cc6c6e5c3ec8
caro-vivi <166158625+caro-vivi@users.noreply.github.com>
2024-05-18 18:43:57 +0200

    Update bot_eco.py

--- bot_eco.py
+++ bot_eco.py
@@ -1,19 +1,18 @@
 import discord
-
+from discord.ext import commandsimport discord
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'Abbiamo fatto l-accesso come {client.user}')
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('raccolta_differenziata'):
-        await message.channel.send("Ti faccio vedere un video per spiegartelo brevemente")
-        video_url = 'https://youtu.be/mDpK_MNOg94'
-        await message.channel.send(video_url)
-
+@bot.command()
+async def raccolta_differenziata(ctx):
+    await ctx.send("Ecco un vidio che ti spiega cosa è la raccolta differenziata:'https://youtu.be/mDpK_MNOg94'")
+@bot.command()
+async def riciclo_plastica(ctx):
+    await ctx.send("Ecco un vidio che ti spiega come si ricicla la plastica:'https://youtu.be/WALo6GLp_to'")
+@bot.command()
+async def riciclo_vetro(ctx):
+    await ctx.send("Ecco un vidio che ti spiega come si ricicla l'alluminio:'https://youtu.be/QSQa1bZ_hGQ'")
     
 bot.run( "scrivi il tuo token" )
